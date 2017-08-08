emacs-pe/password-vault-

commit 0422a3f9a26a2f51095929aba7c9f075200ec9de

Replace APPLY with FUNCALL

password-vault+.el
@@ -93,7 +93,7 @@ marked as dirty upon saving."
         `((name . "Password Vault+")
           (candidates . ,password-vault+-passwords)
           (action . (lambda (candidate)
-                      (apply interprogram-cut-function (list candidate))
+                      (funcall 'interprogram-cut-function candidate)
                       "Password Copied to Clipboard")))))
 
 ;;;###autoload

commit 944ebdeb7e06f0a5f71b27d465c631eb0840f069

Don't quote the symbol we are FUNCALLing
This bug was introduced in 0422a3f9a26a2f51095929aba7c9f075200ec9de

password-vault+.el
@@ -93,7 +93,7 @@ marked as dirty upon saving."
         `((name . "Password Vault+")
           (candidates . ,password-vault+-passwords)
           (action . (lambda (candidate)
-                      (funcall 'interprogram-cut-function candidate)
+                      (funcall interprogram-cut-function candidate)
                       "Password Copied to Clipboard")))))
 
 ;;;###autoload